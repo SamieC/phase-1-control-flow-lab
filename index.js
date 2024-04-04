cec26db51b9f90fd318632a0a49c63b98f9652a2
Sam Chelgut <samchelgut@gmail.com>
2024-04-04 21:09:12 +0300

    assignment done-tests passing

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(x){
+  if (x <= 400)
+  return "This one is on me!"
+
+  else if (x >= 400 && x <= 2000)
+  return "That will be twenty bucks."
+
+  else if (x >= 2000 && x < 2500)
+  return "I will gladly take your thirty bucks."
+
+  else if (x > 2500)
+  return "No can do."
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(y){
+  if (y === "NYC")
+  return "Ok, sounds good."
+
+else if  (y = "NYC")
+return "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(z){
+  if (z === "generous")
+  return  "Thank you so much."
+
+  else if (z === "not as generous")
+  return "Thank you."
+
+  else if (z = "anything else")
+  return "Bye."
 }
